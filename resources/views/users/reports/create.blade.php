e5b73f64ae129f465c2a3a5406e1058effa152ff
Naoyuki Nishiyama <naoyukinishiyama@Naoyukis-MacBook-Air.local>
2026-02-06 20:09:58 +0900

    Update app

--- resources/views/users/reports/create.blade.php
+++ resources/views/users/reports/create.blade.php
@@ -7,10 +7,10 @@
     @csrf
 
         <div class="row g-3 justify-content-center mb-1 mt-3">
-            <div class="col-2 text-end">
+            <div class="col-4 text-end">
                 <label for="year" class="form-label">{{ __('報告年度') }} <span style="font-size: .75em">{{ __('（西暦）')}}</span></label>
             </div>
-            <div class="col-2">
+            <div class="col-4">
                 <input type="text" name="year" id="year" class="form-control text-center" value="">
                 @error('year')
                     <div class="text-danger small">{{ $message }}</div>
@@ -20,10 +20,10 @@
 
         {{-- 主審ブロック以上 --}}
         <div class="row g-3 justify-content-center mb-1">
-            <div class="col-2 text-end">
+            <div class="col-4 text-end">
                 <label for="first_ref_block" class="form-label">{{ __('主審') }} <span style="font-size: .75em">{{ __('（ブロック以上）')}}</span></label>
             </div>
-            <div class="col-2">
+            <div class="col-4">
                 <input type="number" name="first_ref_block" id="first_ref_block" class="form-control text-center w-75" value="">
                 @error('first_ref_block')
                     <div class="text-danger small">{{ $message }}</div>
@@ -37,7 +37,7 @@
                 <label for="second_ref_block" class="form-label">{{ __('副審') }} <span style="font-size: .75em">{{ __('（ブロック以上）')}}</span></label>
             </div>
             <div class="col-4">
-                <input type="number" name="second_ref_block" id="second_ref_block" class="form-control text-center w-25" value="">
+                <input type="number" name="second_ref_block" id="second_ref_block" class="form-control text-center w-75" value="">
                         @error('second_ref_block')
                             <div class="text-danger small">{{ $message }}</div>
                         @enderror
@@ -46,10 +46,10 @@
 
         {{-- 主審都道府県 --}}
         <div class="row g-3 justify-content-center mb-1">
-            <div class="col-2 text-end">
+            <div class="col-4 text-end">
                 <label for="first_ref" class="form-label">{{ __('主審') }} <span style="font-size: .75em">{{ __('（都道府県）')}}</span></label>
             </div>
-            <div class="col-2">
+            <div class="col-4">
                 <input type="number" name="first_ref" id="first_ref" class="form-control text-center w-75" value="">
                 @error('first_ref')
                     <div class="text-danger small">{{ $message }}</div>
@@ -59,10 +59,10 @@
 
         {{-- 副審都道府県 --}}
         <div class="row g-3 justify-content-center mb-1">
-            <div class="col-2 text-end">
+            <div class="col-4 text-end">
                 <label for="second_ref" class="form-label">{{ __('副審') }} <span style="font-size: .75em">{{ __('（都道府県）')}}</span></label>
             </div>
-            <div class="col-2">
+            <div class="col-4">
                 <input type="number" name="second_ref" id="second_ref" class="form-control text-center w-75" value="">
                 @error('second_ref')
                     <div class="text-danger small">{{ $message }}</div>
@@ -72,10 +72,10 @@
 
         {{-- 記録 --}}
         <div class="row g-3 justify-content-center mb-1">
-            <div class="col-2 text-end">
+            <div class="col-4 text-end">
                 <label for="scorer" class="form-label">{{ __('記録') }}</label>
             </div>
-            <div class="col-2">
+            <div class="col-4">
                 <input type="number" name="scorer" id="scorer" class="form-control text-center w-75" value="">
                 @error('scorer')
                     <div class="text-danger small">{{ $message }}</div>
@@ -85,10 +85,10 @@
 
         {{-- AS --}}
         <div class="row g-3 justify-content-center mb-1">
-            <div class="col-2 text-end">
+            <div class="col-4 text-end">
                 <label for="assistant_scorer" class="form-label">{{ __('AS') }}</label>
             </div>
-            <div class="col-2">
+            <div class="col-4">
                 <input type="number" name="assistant_scorer" id="assistant_scorer" class="form-control text-center w-75" value="">
                 @error('assistant_scorer')
                     <div class="text-danger small">{{ $message }}</div>
@@ -98,10 +98,10 @@
 
         {{-- 線審 --}}
         <div class="row g-3 justify-content-center mb-1">
-            <div class="col-2 text-end">
+            <div class="col-4 text-end">
                 <label for="linejudge" class="form-label">{{ __('線審') }}</label>
             </div>
-            <div class="col-2">
+            <div class="col-4">
                 <input type="number" name="linejudge" id="linejudge" class="form-control text-center w-75" value="">
                 @error('linejudge')
                     <div class="text-danger small">{{ $message }}</div>
@@ -111,10 +111,10 @@
 
         {{-- 講習会 --}}
         <div class="row g-3 justify-content-center mb-1">
-            <div class="col-2 text-end">
+            <div class="col-4 text-end">
                 <label for="training" class="form-label">{{ __('講習会') }}</label>
             </div>
-            <div class="col-2">
+            <div class="col-4">
                 <input type="number" name="training" id="training" class="form-control text-center w-75" value="">
                 @error('training')
                     <div class="text-danger small">{{ $message }}</div>
